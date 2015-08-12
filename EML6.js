84ddaee6af2b769b6f62e05743464e9ca7646902
Eduardo Mena <e.menatejeda@gmail.com>
2015-08-11 23:59:23 -0400

    homework 6

---
+++ EML6.js
@@ -0,0 +1,27 @@
+//Selecting Elements
+// $("element to manipulate")
+
+//Anonymous Function
+// function(){
+// 	alert("this is an anonymous")
+// }
+
+//Callbacks
+//$(".obj1").slideUp(1000, function() {
+//$(".obj2").slideDown(1000, function () {
+//slideUp();
+//});
+
+//Document Ready
+//$(document).ready(
+//function(){
+//  alert("it is");
+//}
+//);
+
+//Click event
+//$("element").click(
+//function(){
+//	alert("tick");
+//}
+//);
